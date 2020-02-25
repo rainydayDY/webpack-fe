74e7c2c2d8f5a00461d1dae97f9c9b3b93a97c28
rainydayDY <rainyday66@sina.com>
2020-02-25 10:31:06 +0800

    添加cross-env

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,6 +1,7 @@
 var base = require('./webpack.config');
 const TerserPlugin = require('terser-webpack-plugin'); // 用来压缩js的
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 base.mode = "production";
 base.devtool = 'hidden-source-map';
@@ -8,5 +9,9 @@ base.devtool = 'hidden-source-map';
 base.optimization = {
     minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
 };
+
+if (process.env.ANALYZE) {
+    base.plugins.push(new BundleAnalyzerPlugin());
+}
 
 module.exports = base;
