a483cfba85ef7e51713a4de194afb4a95f51f2f6
rainydayDY <rainyday66@sina.com>
2020-02-26 16:48:17 +0800

    React.lazy使react-hot-loader不生效

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -32,7 +32,7 @@ export default class DetailPage extends React.Component {
                 <h3>be happy jjjjj</h3>
                 <input type="text" placeholder="请输入" value={number1} onChange={(e) => this.handleChange('number1', e.target.value)}/>
                 <input type="text" placeholder="请输入" value={number2} onChange={(e) => this.handleChange('number2', e.target.value)}/>
-                <span>{number1 + number2}</span>
+                {/* <span>{number1 + number2}</span> */}
                 <h1>hello{val}</h1>
                 <h2>lllljjjj</h2>
             </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,4 @@
-// import { lazy } from 'react';
-
-// export default [
-//     {
-//         title: '首页',
-//         key: 'home',
-//         exact: true,
-//         path: '/',
-//         component: lazy(() => import(/* webpackChunkName: 'Home' */'pages/Home')),
-//     },
-//     { 
-//         title: '个人中心页',
-//         path: '/user',
-//         key: 'user',
-//         exact: true,
-//         component: lazy(() => import(/* webpackChunkName: 'User' */'pages/User')),
-//     },
-//     { 
-//         title: '详情页',
-//         key: 'detail',
-//         exact: true,
-//         path: '/detail',
-//         component: lazy(() => import(/* webpackChunkName: 'Detail' */'pages/Detail')),
-//     },
-// ]
-
-import Home from 'pages/Home';
-import User from 'pages/User';
-import Detail from 'pages/Detail';
+import { lazy } from 'react';
 
 export default [
     {
@@ -34,20 +6,48 @@ export default [
         key: 'home',
         exact: true,
         path: '/',
-        component: Home,
+        component: lazy(() => import(/* webpackChunkName: 'Home' */'pages/Home')),
     },
     { 
         title: '个人中心页',
         path: '/user',
         key: 'user',
         exact: true,
-        component: User,
+        component: lazy(() => import(/* webpackChunkName: 'User' */'pages/User')),
     },
     { 
         title: '详情页',
         key: 'detail',
         exact: true,
         path: '/detail',
-        component: Detail,
+        component: lazy(() => import(/* webpackChunkName: 'Detail' */'pages/Detail')),
     },
 ];
+
+// import Home from 'pages/Home';
+// import User from 'pages/User';
+// import Detail from 'pages/Detail';
+
+// export default [
+//     {
+//         title: '首页',
+//         key: 'home',
+//         exact: true,
+//         path: '/',
+//         component: Home,
+//     },
+//     { 
+//         title: '个人中心页',
+//         path: '/user',
+//         key: 'user',
+//         exact: true,
+//         component: User,
+//     },
+//     { 
+//         title: '详情页',
+//         key: 'detail',
+//         exact: true,
+//         path: '/detail',
+//         component: Detail,
+//     },
+// ];
